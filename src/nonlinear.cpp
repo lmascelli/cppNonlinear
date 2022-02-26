a877fcc466e4975c7856bb56a8dce25c16ed7851
Retro Ginocchio <rgtwitch01@gmail.com>
2022-02-26 19:56:00 +0100

    removed old integrate function

--- src/nonlinear.cpp
+++ src/nonlinear.cpp
@@ -84,7 +84,7 @@ mat integrate(SystemDescriptor &system, double t0, mat x0, mat args,
       mat k1 = f(t0 + 0.5 * step, x0 + 0.5 * step * k0, args) * step;
       mat k2 = f(t0 + 0.5 * step, x0 + 0.5 * step * k1, args) * step;
       mat k3 = f(t0 + step, x0 + step * k2, args) * step;
-      x0 += 1. / 6. * (k0 + 2 * k1 + 2 * k2 + k3);
+      x0 += 1. / 6. * (k0 + 2 * k1 + k2 + k3);
       t0 += step;
       if (system.manifold)
       {
@@ -99,61 +99,6 @@ mat integrate(SystemDescriptor &system, double t0, mat x0, mat args,
       }
     }
   }
-  return x0;
-}
-
-mat integrate(system_func f, double t0, mat x0, mat args, double h,
-              uint n_steps, event_func event, EventStruct *result,
-              std::string method)
-{
-  uint current_state;
-  if (event)
-  {
-    current_state = event(t0, x0);
-  }
-
-  h /= (double)n_steps;
-
-  if (method == "euler")
-    for (uint i = 0; i < n_steps; i++)
-    {
-      x0 += h * f(t0, x0, args);
-      if (event)
-      {
-        uint new_state = event(t0, x0);
-        // cout << "integration step progress. cycle " << i
-        //      << " value " << x0 << " system " << new_state << endl;
-        if (new_state != current_state)
-        {
-          result->event = event(t0, x0);
-          result->t = t0;
-          result->x = x0;
-          return x0;
-        }
-      }
-      t0 += h;
-    }
-  else if (method == "rk3")
-    for (uint i = 0; i < n_steps; i++)
-    {
-      mat k0 = f(t0, x0, args) * h;
-      mat k1 = f(t0 + 0.5 * h, x0 + 0.5 * h * k0, args) * h;
-      mat k2 = f(t0 + 0.5 * h, x0 + 0.5 * h * k1, args) * h;
-      mat k3 = f(t0 + h, x0 + h * k2, args) * h;
-      x0 += 1. / 6. * (k0 + 2 * k1 + 2 * k2 + k3);
-      if (event)
-      {
-        uint new_state = event(t0, x0);
-        if (new_state != current_state)
-        {
-          result->event = event(t0, x0);
-          result->t = t0;
-          result->x = x0;
-          return x0;
-        }
-      }
-      t0 += h;
-    }
   return x0;
 }
 
@@ -183,8 +128,7 @@ mat traiectory(SystemDescriptor &system, double t0, mat x0, mat params,
     {
     case SystemDescriptor::EQUATION:
     {
-      x0 = integrate(system.GetFunction(current_label), t0, x0, params, step,
-                     10, system.manifold, &result, "euler");
+      x0 = integrate(system, t0, x0, params, step, &result, 10, method);
 
       if (result.event != current_label)
       {
@@ -204,7 +148,6 @@ mat traiectory(SystemDescriptor &system, double t0, mat x0, mat params,
       x0 = system.GetFunction(current_label)(t0, x0, params);
       t0 += step;
       setCol(i, t0, x0);
-      Log::Print() << "MAP! Cycle: " << i << ", t: " << t0 << ", x: " << x0 << endl;
     }
     break;
     }
--- test.cpp
+++ test.cpp
@@ -5,6 +5,12 @@
 int main(int argc, char const *argv[])
 {
   SystemDescriptor system;
+
+  /**
+   * @brief main system function
+   * dx = a(x^2 + bx+ c - y)
+   * dy = α(βx -y)
+   */
   system.AddFunction(
       [](double t, mat x, mat param)
       {
@@ -17,17 +23,26 @@ int main(int argc, char const *argv[])
         return ret; },
       SystemDescriptor::EQUATION);
 
+  /**
+   * @brief reset rule map
+   *  x -> -55
+   *  y -> y + 4
+   */
   system.AddFunction(
       [](double t, mat x, mat param)
       {
         const double d = -55;
         const double f = 4;
         mat ret{d, x(1) + f};
-        Log::Print() << "Called" << endl;
         return (mat)ret.t();
       },
       SystemDescriptor::MAP);
 
+  /**
+   * @brief the manifold of reset
+   *
+   *  x >= 30
+   */
   system.manifold = [](double, mat x)
   {
     const double xTH = 30;
@@ -35,8 +50,35 @@ int main(int argc, char const *argv[])
     return ret;
   };
 
+  /**
+   * @brief the gradient of the manifold
+   * manifold(x, y) = x - 30
+   * gradient -> (1, 0)
+   */
+  system.manifold_gradient = [](double t, mat x, mat params)
+  {
+    mat ret{1, 0};
+    return ret;
+  };
+
+  /**
+   * @brief the time derivative is always 0
+   *
+   */
+  system.manifold_time_derivative = [](double t, mat x, mat params)
+  {
+    mat ret = arma::zeros(1, 1);
+    return ret;
+  };
+
   mat x0 = {0.5, 5};
   mat m = traiectory(system, 0, x0.t(), {0.1, 1}, 100000, 0.001);
+  constexpr double final_time = 100000 * 0.001;
+
+  mat monodromy = shooting(system, final_time, m.col(m.n_cols - 1), {0.1, 1}, 10.0, 0.001, 10);
+
+  Log::Print() << monodromy << endl;
+
   m.save("data.csv", arma::csv_ascii);
   return 0;
 }
