a112852d2f84b4393ca6e4534f033adc691b80d2
Retro Ginocchio <rgtwitch01@gmail.com>
2022-03-05 20:59:08 +0100

    fixed ps1 script

--- install_run.ps1
+++ install_run.ps1
@@ -1,17 +1,15 @@
 clear
 
 pushd build
-cmake --build .
+cmake --build . --config "Release"
 popd
-
-if ($IsWindows){
-    move -Force build\Release\system_test.dll system_test.pyd
-    move -Force build\src\Release\pynl.dll pynl.pyd
-    python pynl_test.py
-}
 
 if ( $IsLinux){
     move -Force build\libsystem_test.so system_test.so
     move -Force build\src\libpynl.so pynl.so
     python3 pynl_test.py
+} else {
+    move -Force build\Release\system_test.dll system_test.pyd
+    move -Force build\src\Release\pynl.dll pynl.pyd
+    python pynl_test.py
 }
