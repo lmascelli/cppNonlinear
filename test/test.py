b52e23bb0702b012e138c5a0f86fa6d9872bd218
lmascelli <lmascelli@gmail.com>
2022-03-13 12:40:59 +0100

    equ points in vector field

--- test/test.py
+++ test/test.py
@@ -1,12 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from nonlinear import core, plotting
-from typing import List
-
-
-# compiled library imports
-import nonlinear.pynl_bind as nl
+from nonlinear import core, plotting, pynl_bind
+from typing import List, Union, Tuple
 
 
 ######################################################
@@ -15,20 +11,28 @@ import nonlinear.pynl_bind as nl
 #
 ######################################################
 
+a = 0.04
+b = 5.
+c = 150.
+d = -55
+f = 4
+
 
 def f(x: np.ndarray, params: List[float]) -> np.ndarray:
-    a = 0.04
-    b = 5.
-    c = 150.
+
     return np.array([
-        a * np.power(x[0], 2) + b * x[0] + c - x[1],
-        params[0]*(params[1]*x[0]-x[1])
+        # TODO
+    ])
+
+
+def f_jac(x: np.ndarray, params: List[float]) -> np.ndarray:
+    return np.array([
+        [2*a*x[0]-b, -1],
+        [-params[0], params[0]*params[1]]
     ])
 
 
 def map_f(x: np.ndarray, _: List[float]) -> np.ndarray:
-    d = -55
-    f = 4
     return np.array([d, x[1] + f])
 
 
@@ -36,15 +40,90 @@ def manifold(x: np.ndarray) -> int:
     return 1 if x[0] > 30 else 0
 
 
+def equilibrium_points(params: List[float]) -> Tuple[bool, np.ndarray]:
+
+    p1 = (params[1]-b)/(2*a)
+    if (b*b - 2*b*params[1] + params[1]*params[1] - 4 * a*c) > 0:
+        p2 = np.sqrt(b*b - 2*b*params[1] + params[1]*params[1] - 4 * a*c)/(2*a)
+
+        return (
+            True,
+            np.array([
+                [p1+p2, p1-p2],
+                [(p1+p2)*params[1], (p1-p2)*params[1]]
+            ]))
+    else:
+        print(f"for parameters {params} there are no equilibrium points")
+        return (
+            False,
+            np.ndarray(())
+        )
+
+
 ######################################################
 #
 #                   Analysis script
 #
 ######################################################
-params = [0.5, 0.5]
+params = [0.5, 0.2]
 xrange = [-100., 35.]
 yrange = [-100., 100.]
 sampling_points = [30, 30]
+
+
+def test_vector_field(system: Union[core.SystemDescriptor,
+                                    pynl_bind.CompiledSystemDescriptor],
+                      compiled=False):
+
+    fig, ax = plt.subplots()
+    vf: List[List[float]] = []
+
+    def replot():
+        if compiled:
+            vf = pynl_bind.vector_field_2d(system, xrange, yrange,
+                                           sampling_points, params)
+        else:
+            vf = core.vector_field(system, params, xrange, yrange,
+                                   sampling_points)
+        ax.clear()
+        plotting.vector_field(vf, xrange, yrange,
+                              sampling_points, ax)
+        equ_exist, equ = equilibrium_points(params)
+        if equ_exist:
+            ax.scatter(equ[0, :], equ[1, :])
+
+            w1, v1 = np.linalg.eig(f_jac(equ[:, 0], params))
+            w2, v2 = np.linalg.eig(f_jac(equ[:, 1], params))
+
+            ax.quiver(equ[0, 0], equ[1, 0], v1[0, 0], v1[1, 0])
+            ax.quiver(equ[0, 0], equ[1, 0], v1[0, 1], v1[1, 1])
+
+            ax.quiver(equ[0, 1], equ[1, 1], v2[0, 0], v2[1, 0])
+            ax.quiver(equ[0, 1], equ[1, 1], v2[0, 1], v2[1, 1])
+
+        ax.set_title(f'a = {params[0]}, b = {params[1]}')
+        fig.canvas.draw()
+
+    def on_key_press(event):
+        if event.key == 'right' and params[0] < 1:
+            print('Increasing alpha')
+            params[0] += 0.1
+            replot()
+        if event.key == 'left' and params[0] > 0:
+            print('Decreasing alpha')
+            params[0] -= 0.1
+            replot()
+        if event.key == 'up' and params[1] < 1:
+            print('Increasing betha')
+            params[1] += 0.1
+            replot()
+        if event.key == 'down' and params[1] > 0:
+            print('Decreasing betha')
+            params[1] -= 0.1
+            replot()
+
+    fig.canvas.mpl_connect('key_press_event', on_key_press)
+    replot()
 
 
 def analysis():
@@ -59,17 +138,14 @@ def analysis():
     yrange = [-100., 100.]
     sampling_points = [30, 30]
     #### Vector field of the system varying a parameter ###
-    plotting.test_vector_field(system, params,
-                               plotting.Field_Options(
-                                   xrange, yrange, sampling_points))
+    test_vector_field(system)
 
     plt.show()
 
 
 def compiled_analysis():
-    system = nl.GetSystemDescriptor()
-    plotting.test_vector_field(system, params, plotting.Field_Options(
-        xrange, yrange, sampling_points), True)
+    system = pynl_bind.GetSystemDescriptor()
+    test_vector_field(system, True)
     plt.show()
 
 
